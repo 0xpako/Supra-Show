2457f2253e4e4c6e047d21efa342b3bb4717c0c8
Mateusz <77123763+0xpako@users.noreply.github.com>
2023-05-30 00:32:32 +0200

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/Q5-q6d2dggdexkj2/scene.splinecode');
